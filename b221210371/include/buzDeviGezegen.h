1b4a8b5972c630fcd3a43bddd6cd9494530b0bb2
Aybüke Gökçe Yavaş <133589333+Catlas007@users.noreply.github.com>
2025-05-15 16:25:16 +0300

    Add files via upload

---
+++ b221210371/include/buzDeviGezegen.h
@@ -0,0 +1,8 @@
+#ifndef BUZDEVIGEZEGEN_H
+#define BUZDEVIGEZEGEN_H
+
+#include "gezegen.h"
+
+Gezegen* BuzDeviGezegenOlustur(const char* ad, int gun_saat, Zaman tarih);
+
+#endif
---
+++ b221210371/include/uzayAraci.h
@@ -0,0 +1,26 @@
+#ifndef UZAYARACI_H
+#define UZAYARACI_H
+
+#include "zaman.h"
+
+typedef enum { YOLDA, IMHA, HEDEFTE } Durum;
+
+typedef struct UzayAraci {
+    char* ad;
+    char* cikis_gezegeni;
+    char* varis_gezegeni;
+    Zaman cikis_tarihi;
+    int mesafe;
+    int kalan_saat;
+    Zaman varis_tarihi;
+    Durum durum;
+    struct UzayAraci* sonraki;
+} UzayAraci;
+
+UzayAraci* uzayAraciOlustur(const char* ad, const char* c, const char* v, Zaman t, int mesafe);
+UzayAraci* uzayAraclariniYukle(const char* dosya_adi);
+void uzayAraciListesiniYokEt(UzayAraci* bas);
+void uzayAraclariniGuncelle(UzayAraci* bas, const Zaman* gezegenTarih);
+void uzayAraciYazdir(const UzayAraci* a);
+
+#endif
